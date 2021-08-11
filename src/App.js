a4f8136a6b5b2d86fc9c5c69a5704d999e3b1e96
Hannah Norish <hannah@213a.ca>
2021-08-11 18:20:32 -0400

    navbar and smooth-scroll anchor links package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import React from 'react';
+import Navbar from './components/navbar.js';
 import Header from './components/header.js';
 
 
 function App() {
-  
+
+  const links = [
+    { title: 'Info', url: '#about' },
+    { title: 'Projects', url: '#projects' },
+    { title: 'Contact', url: '#contact' }
+  ]
   return (
     <div className="App">
-
+      <Navbar links={links}/>
       <Header />
 
     </div>
